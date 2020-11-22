317475a20414f17cee26450d251505c270408eb2
Parthipan Ramesh <parthipan.ramesh@cispa.saarland>
2020-11-22 15:57:42 +0100

    Add call support (all api levels), bundle support

--- app/src/main/java/saarland/cispa/contentproviderfuzzer/ResolverCaller.kt
+++ app/src/main/java/saarland/cispa/contentproviderfuzzer/ResolverCaller.kt
@@ -3,6 +3,7 @@ package saarland.cispa.contentproviderfuzzer
 import android.content.ContentResolver
 import android.content.ContentValues
 import android.net.Uri
+import android.os.Bundle
 import saarland.cispa.cp.fuzzing.serialization.*
 
 class ResolverCaller(private val resolver: ContentResolver) {
@@ -10,7 +11,14 @@ class ResolverCaller(private val resolver: ContentResolver) {
     fun process(data: ContentProviderApi) {
         val uri = Uri.parse(data.uri)
         when (data) {
-            is ResolverCallUri -> resolver.call(uri, data.method, data.arg, null)
+            is ResolverCallUri -> {
+                val bundle = parseBundleKey(data.extras)
+                when (data.apiLevel) {
+                    CallApiLevel.API_11 -> resolver.call(uri, data.method, data.arg, bundle)
+                    CallApiLevel.API_29 -> resolver.call(data.uri, data.method, data.arg, bundle)
+                }
+
+            }
 
             is ResolverQueryApi1 -> {
                 resolver.query(
@@ -43,5 +51,22 @@ class ResolverCaller(private val resolver: ContentResolver) {
                 resolver.delete(uri, data.selection, null)
             }
         }
+    }
+
+    private fun parseBundleKey(extras: BundleKey?): Bundle? {
+        if (extras == null) {
+            return null
+        }
+
+        val bundle = Bundle()
+        when (extras.type) {
+            JavaType.STRING -> bundle.putString(extras.key, null)
+            JavaType.INT -> bundle.putInt(extras.key, 0)
+            JavaType.LONG -> bundle.putLong(extras.key, 0)
+            JavaType.BOOL -> bundle.putBoolean(extras.key, true)
+            JavaType.OBJECT -> bundle.putSerializable(extras.key, null)
+            JavaType.BYTES -> bundle.putByteArray(extras.key, null)
+        }
+        return bundle
     }
 }
